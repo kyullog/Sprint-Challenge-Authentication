e56f503df802f738c68c5a76283caf2ad4fcc162
Jonathan Dorety <kyullog@gmail.com>
2019-04-06 12:29:45 -0400

    Configure basic register api endpoint

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,27 @@
-const axios = require('axios');
+const axios = require("axios");
 
-const { authenticate } = require('../auth/authenticate');
+const { authenticate } = require("../auth/authenticate");
+const db = require("../database/dbHelpers.js");
+
+const genericError = { error: "There was a problem processing your request" };
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
-function register(req, res) {
+async function register(req, res) {
   // implement user registration
+  try {
+    const user = req.body;
+    const [newUserId] = await db.registerUser(user);
+    if (newUserId) {
+      res.status(201).json(newUserId);
+    }
+  } catch {
+    res.status(500).json(genericError);
+  }
 }
 
 function login(req, res) {
@@ -18,15 +30,15 @@ function login(req, res) {
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: "application/json" }
   };
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
+    .get("https://icanhazdadjoke.com/search", requestOptions)
     .then(response => {
       res.status(200).json(response.data.results);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
